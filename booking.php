b877a546f9b3502e7047dd8110441f50d027efa5
sisirrag <mca549@rajagiri.edu>
2021-05-30 23:26:05 +0530

    m

--- booking.php
+++ booking.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>booking</title>
+    <title>Rider's Cafe:</title>
 </head>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
